21ae38766fc5e46629c04f2c1d33bbf58763ac0d
deepcloudlabs <deepcloudlabs@gmail.com>
2021-02-11 11:47:00 +0300

    call lottery module

--- lottery-nodejs/service.js
+++ lottery-nodejs/service.js
@@ -1,7 +1,17 @@
 let http = require('http');
-const port = 9000;
+let lottery = require('lottery');
+
+const port = 9200;
+/*
 http.createServer(function(req,res){
     res.writeHead(200);
-    res.end(JSON.stringify([4,8,15,16,23,42]));
+    lottery.draw(50,6).then( nums => res.end(JSON.stringify(nums)) )
 }).listen(port);
+*/
+http.createServer(async (req,res) => {
+    res.writeHead(200);
+    let nums = await lottery.draw(50,6);
+    res.end(JSON.stringify(nums));
+}).listen(port);
+
 console.log(`Listening on port ${port}`);
