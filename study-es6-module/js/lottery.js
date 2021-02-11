4892b243e1359895af6983198080f8f33b270778
deepcloudlabs <deepcloudlabs@gmail.com>
2021-02-11 10:15:59 +0300

    initial commit

---
+++ study-es6-module/js/lottery.js
@@ -0,0 +1,10 @@
+export async function draw(max=50,size=6){
+    let numbers = [];
+    while (numbers.length<size){
+        let number = Math.floor(Math.random()*max)+1;
+        if (numbers.includes(number)) continue;
+        numbers.push(number);
+    }
+    numbers.sort((x,y)=>x-y);
+    return numbers;
+}
