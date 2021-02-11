b2f10218e46a894af8657c16fc17966436d398cc
deepcloudlabs <deepcloudlabs@gmail.com>
2021-02-11 17:38:34 +0300

    fix validation utility functions fix import problem add swagger ui configuration

--- hr-backend/service.js
+++ hr-backend/service.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
-import utils from "./utils"
+require("./utils");
 
+//region mongodb
 // 1. mongodb connection
 const mongodb_url = "mongodb://localhost:27017/hrdb";
+const collection_name = 'employees';
 const mongo_opts = {
     "useNewUrlParser": true,
     "socketTimeoutMS": 0,
@@ -26,6 +28,7 @@ const employeeSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        validation: /^[1-9][0-9]{10}$/,
         validate: [
             tcKimlikNoValidator,
             "You must provide a valid identity no."
@@ -62,9 +65,45 @@ const employeeSchema = new mongoose.Schema({
         enum: ["IT", "Sales", "Finance", "HR"],
         required: false,
         default: "Finance"
+    },
+    "version": {
+        type: String,
+        required: false,
+        default: "1.2.0"
     }
 });
 // 3. CRUD Persistence Object
-const Employee = mongoose.model('employees',employeeSchema );
+const Employee = mongoose.model(collection_name,employeeSchema );
+//endregion
+
+//region express + swagger ui configuration
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const swaggerUi = require("swagger-ui-express");
+const openApiDoc = require("./swagger-hr");
+
+
+const port = 7100;
+const api = express();
+api.use(bodyParser.json({limit : "3mb"}));
+api.use(logger('dev'));
+// CORS Filter
+api.use(function(req,res,next){
+   res.header("Access-Control-Allow-Origin","*");
+   res.header("Access-Control-Allow-Methods", "HEAD, POST, PUT, DELETE, PATCH, GET");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+   next();
+})
+api.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openApiDoc))
+//endregion
+
+//region rest over http
+
+//endregion
+
+//region rest over ws
+
+//endregion
 
 console.log("Server is up and running...")
